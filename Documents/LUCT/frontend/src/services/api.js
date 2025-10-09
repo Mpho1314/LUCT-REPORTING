d69120a117aa68937ea1cfb2b317af5368fab833
Mpho Qaba <mphoqaba1@gmail.com>
2025-10-09 16:04:19 +0200

    Update api.js

--- Documents/LUCT/frontend/src/services/api.js
+++ Documents/LUCT/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://reporting-system-5.onrender.com';
 
 // Create axios instance with base configuration
 const api = axios.create({
