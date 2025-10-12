a422feca1a5b0a9ac8623729cc92dfc904bf686d
Mpho Qaba <mphoqaba1@gmail.com>
2025-10-12 21:31:06 +0200

    Update auth.js

--- reporting-system-backend/routes/auth.js
+++ reporting-system-backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
 const db = require('../db/connection'); // ✅ correct path
-
+const API_URL = process.env.REACT_APP_API_URL;
 
 
 router.post('/register', register);
