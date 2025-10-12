7cbfe74a1fd3e6124eb1844638b61b396c63d12f
Mpho Qaba <mphoqaba1@gmail.com>
2025-10-12 21:32:44 +0200

    Update lectures.js

--- reporting-system-backend/routes/lectures.js
+++ reporting-system-backend/routes/lectures.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/connection'); // ✅ correct path
+const API_URL = process.env.REACT_APP_API_URL;
 const { submitLecture, getLecturesByCourse } = require('../controllers/lectureController');
 
 // ------------------------
